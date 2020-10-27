f9ff71c28cb72f1eba0981583d9bb8aab87fa241
laurentPause <48903538+laurentPause@users.noreply.github.com>
2020-10-27 09:05:13 +0400

    historiques

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 addOnEcran();
 AddOperator();
-calul();
+calcul();
+displayHistorique();
 function addOnEcran() {
     const zero = document.getElementById('0');
     const un = document.getElementById('1');
@@ -12,34 +13,34 @@ function addOnEcran() {
     const sept = document.getElementById('7');
     const huit = document.getElementById('8');
     const neuf = document.getElementById('9');
-    
-    zero.addEventListener('click',()=> addNb(0));
-    un.addEventListener('click', ()=> addNb(1));
-    deux.addEventListener('click', ()=> addNb(2));
-    trois.addEventListener('click',()=> addNb(3));
-    quatre.addEventListener('click',()=> addNb(4));
-    cinq.addEventListener('click',()=> addNb(5));
-    six.addEventListener('click',()=> addNb(6));
-    sept.addEventListener('click',()=> addNb(7));
-    huit.addEventListener('click',()=> addNb(8));
-    neuf.addEventListener('click', ()=> addNb(9));
+
+    zero.addEventListener('click', () => addNb(0));
+    un.addEventListener('click', () => addNb(1));
+    deux.addEventListener('click', () => addNb(2));
+    trois.addEventListener('click', () => addNb(3));
+    quatre.addEventListener('click', () => addNb(4));
+    cinq.addEventListener('click', () => addNb(5));
+    six.addEventListener('click', () => addNb(6));
+    sept.addEventListener('click', () => addNb(7));
+    huit.addEventListener('click', () => addNb(8));
+    neuf.addEventListener('click', () => addNb(9));
 }
 
 function addNb(nb) {
-    const ecran =  document.getElementById('ecran');
+    const ecran = document.getElementById('ecran');
     const minEcran = document.getElementById('minEcran');
     const NB = document.getElementById('NB');
-    const dernier =  minEcran.value[minEcran.value.length - 1];
-    if((dernier == '+' || dernier == '-' || dernier == '*' || dernier == '/')  && NB.value == 0){
+    const dernier = minEcran.value[minEcran.value.length - 1];
+    if ((dernier == '+' || dernier == '-' || dernier == '*' || dernier == '/') && NB.value == 0) {
         ecran.value = 0;
         NB.value = 1
     }
-    if(NB.value == 2){
+    if (NB.value == 2) {
         ecran.value = 0
         NB.value = 0
     }
     console.log(dernier);
-    if( ecran.value == 0){
+    if (ecran.value == 0) {
         ecran.value = '';
     }
     ecran.value += nb;
@@ -51,10 +52,10 @@ function AddOperator() {
     const addition = document.getElementById('A');
     const soustraction = document.getElementById('S');
 
-    division.addEventListener('click',()=> addOp('/'));
-    multiplication.addEventListener('click',()=> addOp('*'));
-    addition.addEventListener('click',()=> addOp('+'));
-    soustraction.addEventListener('click',()=> addOp('-'));
+    division.addEventListener('click', () => addOp('/'));
+    multiplication.addEventListener('click', () => addOp('*'));
+    addition.addEventListener('click', () => addOp('+'));
+    soustraction.addEventListener('click', () => addOp('-'));
 }
 
 function addOp(operator) {
@@ -68,18 +69,18 @@ function addOp(operator) {
 
 }
 
-function calul(){
+function calcul() {
     const egal = document.getElementById('E');
     const ecran = document.getElementById('ecran');
     const minEcran = document.getElementById('minEcran');
     const NB = document.getElementById('NB');
 
-    egal.addEventListener('click',()=>{
+    egal.addEventListener('click', () => {
         if (minEcran.value != '') {
-           const aCalculer = minEcran.value.split(' ');
-           console.log(aCalculer);
-           let number = 0;
-           aCalculer.forEach(element => {
+            const aCalculer = minEcran.value.split(' ');
+            console.log(aCalculer);
+            let number = 0;
+            aCalculer.forEach(element => {
                 switch (element) {
                     case '+':
                         number = number + parseFloat(ecran.value)
@@ -93,17 +94,54 @@ function calul(){
                     case '*':
                         number = number * parseFloat(ecran.value)
                         break;
-                
+
                     default:
                         number = parseFloat(element);
                         break;
                 }
-            
-           });
-           ecran.value = number;
-           minEcran.value = '';
-           NB.value = 2;
+
+            });
+            addHistorique(ecran.value, minEcran.value, number);
+            ecran.value = number;
+            minEcran.value = '';
+            NB.value = 2;
         }
     })
+}
+
+function addHistorique(ecran, minEcran, result) {
+    const historique = localStorage.getItem('calculs');
+    const calcul = minEcran + ' ' + ecran + ' = ' + result;
+
+    if (historique) {
+        const calculs = JSON.parse(historique);
+        if (calculs.length < 10) {
+            calculs.push(calcul);
+            const calculString = JSON.stringify(calculs);
+            localStorage.setItem('calculs', calculString);
+        }else{
+            calculs[0] = calcul;
+            const calculString = JSON.stringify(calculs);
+            localStorage.setItem('calculs', calculString);
+        }
+    } else {
+        const calculs = [];
+        calculs.push(calcul);
+        const calculString = JSON.stringify(calculs);
+        localStorage.setItem('calculs', calculString);
+    }
+    displayHistorique();
+}
+
+function displayHistorique() {
+    const listeHistorique = document.getElementById('liste-historique');
+    const historique = localStorage.getItem('calculs');
+    const calculs = JSON.parse(historique);
+    listeHistorique.innerHTML = '';
+    calculs.forEach(calcul => {
+        const li = document.createElement('li');
+        li.innerHTML = calcul;
+        listeHistorique.append(li);
+    });
 }
 
